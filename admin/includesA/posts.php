8decb54013b02ca585302ef815fae23f7c2948e0
Tim <tferris88@gmail.com>
2018-05-02 19:29:39 -0400

    i love my sense of humor sometimes

---
+++ admin/includesA/posts.php
@@ -0,0 +1,63 @@
+<?php include "header.php"; ?>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include "navigation.php"; ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+
+                    <h1 class="page-header">
+                            Welcome Oh-<strong>Mighty</strong>-One...
+                            <small>Tim</small>
+                        </h1>
+
+                        <table class = "table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Author</th>
+                                    <th>Title</th>
+                                    <th>Category</th>
+                                    <th>Status</th>
+                                    <th>Image</th>
+                                    <th>Tags</th>
+                                    <th>Comments</th>
+                                    <th>Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>N</td>
+                                    <td>Someone important...</td>
+                                    <td>Something important...</td>
+                                    <td>Meh</td>
+                                    <td>ill get around to it</td>
+                                    <td>i love pictures!</td>
+                                    <td>tag?  sweet!</td>
+                                    <td>say it...I dare you...</td>
+                                   <td>that one time at band camp...</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+   <?php include "footer.php"; ?>
