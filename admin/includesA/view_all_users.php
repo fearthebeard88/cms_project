588d291f933d916efe35315bb9eae985f4b88a72
Tim <tferris88@gmail.com>
2018-07-28 16:23:31 -0400

    man its been a while since ive worked on this...

--- admin/includesA/view_all_users.php
+++ admin/includesA/view_all_users.php
@@ -1,13 +1,15 @@
 <table class = "table table-bordered table-hover">
                             <thead>
                                 <tr>
-                                    <th>User Id</th>
                                     <th>Username</th>
                                     <th>First Name</th>
                                     <th>Last Name</th>
                                     <th>Email</th>
-                                    <th>Role</th>
+                                    <th>Title</th>
                                     <th>Date</th>
+                                    <th>To Admin</th>
+                                    <th>To Subscriber</th>
+                                    <th>Delete</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -30,14 +32,15 @@ while ($row = mysqli_fetch_assoc($show_all)) {
     $image = $row['user_img'];
 // putting variables into table format 
     echo "<tr>";
-    echo "<td>$id</td>";
     echo "<td>$username</td>";
     echo "<td>$first_name</td>";
     echo "<td>$last_name</td>";
     echo "<td>$email</td>";
     echo "<td>$role</td>";
     echo "<td>$date</td>";
-    echo "<td><a href = 'users.php?delete={$id}'>Delete</a></td>";
+    echo "<td><a href='users.php?change_admin={$id}'>Make Admin</td>";
+    echo "<td><a href='users.php?change_sub={$id}'>Make Subscriber</td>";
+    echo "<td><a href = 'users.php?delete={$id}'>delete</a></td>";
 // selecting all from table comments where the comment_id = $id from comments table
 // $query = "SELECT * FROM categories WHERE cat_id = {$category} ";
 // $select_cat_id = mysqli_query($connect, $query);
@@ -64,20 +67,20 @@ $delete = mysqli_query($connect, $query);
 header("Location: users.php");
 }
 
-if (isSet($_GET['unapprove'])) {
-    $id = $_GET['unapprove'];
+if (isSet($_GET['change_admin'])) {
+    $id = $_GET['change_admin'];
 
-    $query = "UPDATE comments SET comment_status = 'Unapproved' WHERE comment_id = $id ";
+    $query = "UPDATE users SET role = 'admin' WHERE user_id = $id ";
     $unapprove = mysqli_query($connect, $query);
-    header("Location: comments.php");
+    header("Location: users.php");
 }
 
-if (isSet($_GET['approve'])) {
-    $id = $_GET['approve'];
+if (isSet($_GET['change_sub'])) {
+    $id = $_GET['change_sub'];
 
-    $query = "UPDATE comments SET comment_status = 'Approved' WHERE comment_id = $id ";
+    $query = "UPDATE users SET role = 'subscriber' WHERE user_id = $id ";
     $approve = mysqli_query($connect, $query);
-    header("Location: comments.php");
+    header("Location: users.php");
 }
 
 ?>
